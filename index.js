f8414bb2a4b67cffeadfce41fb7ec4349bcf9355
internetvandingen <internetvandingen@protonmail.com>
2019-04-19 21:26:01 +0200

    fix bug where last chat message was not displaying

--- index.js
+++ index.js
@@ -19,22 +19,24 @@ app.get("/lobby/", function(req, res) {
 });
 app.use('/lobby/', express.static(__dirname + '/public/'));
 
-var max_nr_players = 2;
-var players = {};
+var max_nr_players = 50;
+var players = {}; // {'id':#, 'gameid':#}
 var lobby = [];
 
 
-function game(name, max_players, map) {
+function Game(name, max_players, map) {
   this.id = (lobby.length==0 ? 0 : lobby[lobby.length-1].id+1);
   this.name = name;
   this.player_count = 0;
+  this.players = {};
   this.max_players = max_players;
   this.board = {};
   this.map = map;
+  this.turn = 1;
 }
 
-lobby.push(new game('Lennarts game', 2, 'map1'));
-lobby.push(new game('Marks game', 3, 'map1'));
+lobby.push(new Game('Lennarts game', 2, 'map1'));
+lobby.push(new Game('Marks game', 3, 'map1'));
 
 
 io.on('connection', function(socket) {
@@ -42,16 +44,16 @@ io.on('connection', function(socket) {
   if (Object.keys(players).length < max_nr_players){
     let current_players = [];
     for (let i in players){
-      current_players.push(players[i]);
+      current_players.push(players[i].id);
     }
     for (let i=1; i<=max_nr_players; i++){
       if (!current_players.includes(i)){
-        players[socket.id] = i;
+        players[socket.id] = {'id':i, 'gameid':-1};
         break;
       }
     }
-    socket.emit('player', players[socket.id]);
-    io.emit('chat message', {id:'player '+players[socket.id], message:' connected', color:players[socket.id]});
+    socket.emit('player', players[socket.id].id);
+    io.emit('chat message', {id:'player '+players[socket.id].id, message:' connected', color:players[socket.id].id});
   } else {
     // disallow player and disconnect
     socket.emit('chat message', {id:'', message:'Lobby is full! ('+max_nr_players+')', color:0});
@@ -75,13 +77,13 @@ io.on('connection', function(socket) {
   });
 
   socket.on('new game', function(){
-    console.log('player '+players[socket.id]+'made new game');
+    console.log('player '+players[socket.id].id+' made new game');
     attempt_create_game();
   });
 
   socket.on('disconnect', function() {
     if (Object.keys(players).includes(socket.id)){
-      io.emit('chat message', {id:'player '+players[socket.id], message:' disconnected', color:players[socket.id]});
+      io.emit('chat message', {id:'player '+players[socket.id].id, message:' disconnected', color:players[socket.id].id});
       delete players[socket.id];
     } else {
       io.emit('chat message', {id:'', message:'Unknown player disconnected', color:0});
@@ -91,8 +93,8 @@ io.on('connection', function(socket) {
 
 function recieved_chat_message(msg, socketid){
   if (msg != ''){
-    let temp_name = Object.keys(players).includes(socketid) ? 'player '+players[socketid] : 'spectator '+spectators[socketid] ;
-    io.emit('chat message', {id:temp_name+': ', message:msg, color:players[socketid]});
+    let temp_name = Object.keys(players).includes(socketid) ? 'player '+players[socketid].id : 'spectator '+spectators[socketid] ;
+    io.emit('chat message', {id:temp_name+': ', message:msg, color:players[socketid].id});
   }
 }
 
@@ -102,6 +104,11 @@ function attempt_join(socket_id, game_id) {
 }
 
 function attempt_create_game() {
-//  lobby.push(new game('Lennarts game', 2, 'map1'));
+  lobby.push(new Game('Lennarts game', 2, 'map1'));
+}
+
+function join_game(game, socketid, gameid) {
+  players[socketid].gameid = gameid;
+  game.players[socketid] = players[socketid];
 }
 
